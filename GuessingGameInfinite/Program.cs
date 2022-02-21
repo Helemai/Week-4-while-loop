0f556ef26781ad6202b148a3fa5a586c2dbfef8e
Hele-Mai Ristmäe <hristmae@tktk.ee>
2022-02-21 20:07:54 +0200

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhusliku numbrit,1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis kasutaja on mängu võitnud
+            //katsete arv on piiramatu
+            //programm genereerib juhuslikku numbrit vaid ühe korra.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            
+            bool loopActive = true;
+
+            
+            while (loopActive)
+            {
+                Console.WriteLine("Sisesta number:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if(userNumber == cpuNumber)
+                {
+                    Console.WriteLine($"Juhuslik number on {cpuNumber}. Palju õnne! Oled võitnud!");
+                    loopActive = false;
+                    Console.WriteLine("Kena päeva!");
+                }
+                else
+                {
+                    Console.WriteLine("Vale number, proovi uuesti:");
+                }
+                
+            }
+
+
+
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhusliku numbrit,1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis kasutaja on mängu võitnud
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata, siis mängu võidab arvuti.
+            //programm genereerib juhuslikku numbrit vaid ühe korra.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+
+            while (i < 3) 
+            {
+                Console.WriteLine("Sisesta number");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (cpuNumber == userNumber)
+                {
+                    Console.WriteLine($"Juhuslik number on {cpuNumber}. Palju õnne, võitsid mängu!");
+                    break;
+                }
+                else 
+                { 
+                    i = i + 1;
+                        Console.WriteLine($"Vale number {3 - i} katset jäänud.");
+
+                    }              
+        
+
+                
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
